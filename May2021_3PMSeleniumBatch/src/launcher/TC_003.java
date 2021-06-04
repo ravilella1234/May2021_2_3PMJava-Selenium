8de2aa180eea1d813f178494ae3791c22b15b4b2
maha <maha@gmail.com>
2021-06-04 15:56:14 +0530

    Log4j & Extent Reports

--- May2021_3PMSeleniumBatch/src/launcher/TC_003.java
+++ May2021_3PMSeleniumBatch/src/launcher/TC_003.java
@@ -1,21 +1,30 @@
 package launcher;
 
+import org.apache.log4j.Logger;
+
 public class TC_003 extends BaseTest
 {
-
+	private static final Logger log = Logger.getLogger(TC_003.class);
+	
 	public static void main(String[] args) throws Exception
 	{
 		init();
+		log.info("Loading the Files......");
 		
 		launch("chromebrowser");
+		log.info("Launched the Browser :- " + p.getProperty("chromebrowser"));
 		
 		navigateUrl("amazonurl");
+		log.info("Navigated to url :- " +  childProp.getProperty("amazonurl"));
 		
 		selectOption("amazondropbox_id","Books");
+		log.info("Selected the option Books By using Locator :- " + orProp.getProperty("amazondropbox_id"));
 		
 		typeText("amazonsearchtext_id","Harry Potter");
+		log.info("Entered the text HArry Potter By using Locator :- " + orProp.getProperty("amazonsearchtext_id"));
 		
 		clickElement("amazonsearchbutton_xpath");
+		log.info("Clicked on Amazon Search Button By using Locaotr :- " + orProp.getProperty("amazonsearchbutton_xpath"));
 		
 		
 		//WebElement loc = driver.findElement(By.id("searchDropdownBox"));
